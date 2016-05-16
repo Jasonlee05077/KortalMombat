566edb3a25afeb9f605289545bb9d54d588602e6
linj <jonathan.lin1138@gmail.com>
2016-05-16 07:52:54 -0700

    fgaaa

--- app/src/main/java/com/example/linj/kortalmombat/MainActivity.java
+++ app/src/main/java/com/example/linj/kortalmombat/MainActivity.java
@@ -10,4 +10,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+EditText1 editSpecial = (EditText) findViewById(R.id.editText);
+    editSpecial
+
+
+
 }
