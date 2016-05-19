1339501b5759f0c509fbe54dcb0935a04d792bb6
leej2 <jason1998.jl@gmail.com>
2016-05-19 07:31:19 -0700

    Added FightingActivity

---
+++ app/src/main/java/com/example/linj/kortalmombat/FightingActivity.java
@@ -0,0 +1,13 @@
+package com.example.linj.kortalmombat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FightingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fighting);
+    }
+}
