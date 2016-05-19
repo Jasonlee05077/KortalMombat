59c2a8bdfe36454b9e865c5e8c5669c41b721b35
linj <jonathan.lin1138@gmail.com>
2016-05-19 08:17:13 -0700

    gdfgxd

--- app/src/main/java/com/example/linj/kortalmombat/MainActivity.java
+++ app/src/main/java/com/example/linj/kortalmombat/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = (Button) findViewById(R.id.button);
+        Button continueButton = (Button) findViewById(R.id.button);
         Button.OnClickListener(new View.OnClickListener){
             public void onClick(View v){
                 EditText specialText = ((EditText) findViewById(R.id.editText));
