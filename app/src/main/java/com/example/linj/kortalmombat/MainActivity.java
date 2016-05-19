1844576d5c5686a62e8c03e5658fade87d9b0745
linj <jonathan.lin1138@gmail.com>
2016-05-19 08:12:31 -0700

    gdfgxd

--- app/src/main/java/com/example/linj/kortalmombat/MainActivity.java
+++ app/src/main/java/com/example/linj/kortalmombat/MainActivity.java
@@ -6,35 +6,37 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.view.View.OnClickListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        Button.OnClickListener(new View.OnClickListener){
+            public void onClick(View v){
+                EditText specialText = ((EditText) findViewById(R.id.editText));
+                String specialString = specialText.getText().toString();
+                final int special = Integer.parseInt(specialString);
+                EditText attackText = ((EditText) findViewById(R.id.editText2));
+                String attackString = attackText.getText().toString();
+                final int attack = Integer.parseInt(attackString);
+                EditText defenseText = ((EditText) findViewById(R.id.editText3));
+                String defenseString = defenseText.getText().toString();
+                final int defense = Integer.parseInt(defenseString);
+                int totalPoints = special + attack + defense;
+                if (totalPoints != 30){
+                    System.out.println("Please use 30 points");
+                }
+                else {startActivity(new Intent(MainActivity.this,FightingActivity.class));}
 
+                }
+            }
 
     }
 
-    private int special = ((EditText) findViewById(R.id.editText);
-    private int attack = ((EditText) findViewById(R.id.editText2);;
-    private int defense = ((EditText) findViewById(R.id.editText3);;
-    private Button continue_button = (Button) findViewById(R.id.button);
-    private Button button_click = (Button) findViewById(R.id.button);
-    private int totalPoints = special + attack + defense;
 
-    public void finished_char(int totalPoints){
-        if (totalPoints != 30){
-           System.out.println("Please use 30 points");
-        }
-        else {startActivity(new Intent(MainActivity.this,FightingActivity.class));}
-
-    }
-
-    button_click.setOnClickListener(new View.OnClickListener(){
-        finished_char(totalPoints);
-    }
 }
 
