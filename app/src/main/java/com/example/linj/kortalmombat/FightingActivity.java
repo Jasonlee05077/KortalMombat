c8a2689209e67925588cfa35b35bddaffef78501
linj <jonathan.lin1138@gmail.com>
2016-05-24 08:19:23 -0700

    gdfgxd

--- app/src/main/java/com/example/linj/kortalmombat/FightingActivity.java
+++ app/src/main/java/com/example/linj/kortalmombat/FightingActivity.java
@@ -2,6 +2,15 @@ package com.example.linj.kortalmombat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+
+
 
 public class FightingActivity extends AppCompatActivity {
 
@@ -11,7 +20,13 @@ public class FightingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_fighting);
 
 
-        public void set
+        myLayout.setOnTouchListener(
+                new RelativeLayout.OnTouchListener() {
+                    public boolean onTouch(View v, MotionEvent m) {
+                        // Perform tasks here
+                        return true;
+                    }
+                }
     }
 
 
