0c0f6ea70c01913f4f0a2fa6f0a83d0edc2f82e1
linj <jonathan.lin1138@gmail.com>
2016-05-26 10:59:18 -0700

    fighting activity started

--- app/src/main/java/com/example/linj/kortalmombat/Fighter.java
+++ app/src/main/java/com/example/linj/kortalmombat/Fighter.java
@@ -82,6 +82,14 @@ public class Fighter {
     public void setSpecial(int special) {
         this.special = special;
     }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
 }
 
 
--- app/src/main/java/com/example/linj/kortalmombat/FightingActivity.java
+++ app/src/main/java/com/example/linj/kortalmombat/FightingActivity.java
@@ -9,18 +9,44 @@ import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-
+import java.util.Random;
 
 
 public class FightingActivity extends AppCompatActivity {
+    private boolean TwoFingersTapped;
+    private boolean hitpercent;
+    Random random = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fighting);
+    button.setOnTouchListener(new View.OnTouchListener() {
+                                  public boolean onTouch(View view, MotionEvent motionEvent) {
+                                      int action = motionEvent.getAction();
+                                      Switch(action & MotionEvent.ACTION_MASK) {
+                                          caseMotionEvent.ACTION_POINTER_DOWN;
+                                          TwoFingersTapped = true;
+                                          if (YourFighter.getFighter().getSpecial() + YourFighter.getFighter().getAttack() >= 21) {
+                                              int place = random.nextInt(100);
+                                              if (place > 10) {
+                                                  ComputerFighter.getFighter().setHp(ComputerFighter.getFighter().getHp() - YourFighter.getFighter().getSpecial());
+                                              }
+                                          } else if (YourFighter.getFighter().getSpecial() + YourFighter.getFighter().getAttack() >= 10) {
+                                              int place = random.nextInt(100);
+                                              if (place > 25) {
+                                                  ComputerFighter.getFighter().setHp(ComputerFighter.getFighter().getHp() - YourFighter.getFighter().getSpecial());
+                                              }
+                                          } else if (YourFighter.getFighter().getSpecial() + YourFighter.getFighter().getAttack() < 10) {
+                                              int place = random.nextInt(100);
+                                              if (place > 50) {
+                                                  ComputerFighter.getFighter().setHp(ComputerFighter.getFighter().getHp() - YourFighter.getFighter().getSpecial());
+                                              }
+                                          }
 
+                                      }}
 
-        myLayout.setOnTouchListener(
+        /*myLayout.setOnTouchListener(
                 new RelativeLayout.OnTouchListener() {
                     public boolean onTouch(View v, MotionEvent m) {
                         // Perform tasks here
@@ -28,7 +54,9 @@ public class FightingActivity extends AppCompatActivity {
                     }
                 }
     }
+*/
 
+    public void GestureDetector(){
 
-
+    }
 }
