5fcc8b116f06dc777a01857b65f1ad727f55d5c7
leej2 <jason1998.jl@gmail.com>
2016-06-07 07:31:52 -0700

    update

--- app/src/main/java/com/example/linj/kortalmombat/ComputerFighter.java
+++ app/src/main/java/com/example/linj/kortalmombat/ComputerFighter.java
@@ -4,7 +4,7 @@ package com.example.linj.kortalmombat;
  * Created by jason on 5/23/2016.
  */
 public class ComputerFighter {
-    private static Fighter fighter = new Fighter(0,0,0,"0","0");
+    private static Fighter fighter = new Fighter(0,0,0,"random","nothing");
 
     public static Fighter getFighter() {
         return fighter;
